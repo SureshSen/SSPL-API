e829269e9f1b8bc00afe34d75a547ad35785382e
SureshSen <115155721+SureshSen@users.noreply.github.com>
2023-05-16 11:29:59 +0530

    Update conversationController.js

--- src/controller/conversationController.js
+++ src/controller/conversationController.js
@@ -5,20 +5,15 @@ const sendMail = require('../utility/sendEmail');
 
 //create conversation
 const create = async (req, res) => {   
-    console.log('create method call');
     try {
-        client.query("INSERT INTO conversations(uuid,created_at,modified_at,conversation)VALUES($1,now(),now(),$2) RETURNING *", [uuidv4(), req.body], (error, result) => {
+        client.query("INSERT INTO conversations(uuid,created_at,modified_at,conversation)VALUES($1,now(),now(),$2) RETURNING *", [uuidv4(), req.body], async(error, result) => {
             if (!error) {
-                res.status(201).json({ data: result.rows[0] });
-                // if (sendMail())
-                //     res.status(201).json({ data: result.rows[0] });
-                // else {
-                //     res.status(500).json({ message: "Something went wron on email sent" });
-                // }
+                await sendMail();
+                res.status(201).json({ data: result.rows[0] });               
             }
             else {
                 res.status(500).json({ message: error });
-            }            
+            }
         });
     } catch (error) {
         console.log('catch error');
@@ -26,17 +21,13 @@ const create = async (req, res) => {
     }
 };
 //update conversation :uuid
-const update = async (req, res) => {    
+const update = async (req, res) => {
     const { uuid } = req.params;    
     try {
-         client.query("UPDATE conversations SET modified_at=now(),conversation=$1 WHERE uuid = $2 RETURNING *", [req.body, uuid], (error, result) => {
+        client.query("UPDATE conversations SET modified_at=now(),conversation=$1 WHERE uuid = $2 RETURNING *", [req.body, uuid], async(error, result) => {
             if (!error) {
-                res.status(201).json({ data: result.rows[0] });
-                // if (sendMail())
-                //     res.status(201).json({ data: result.rows });
-                // else {
-                //     res.status(500).json({ message: "Something went wron on email sent" });
-                // }
+                await sendMail(email);
+                res.status(201).json({ data: result.rows[0] });                
             }
             else {
                 res.status(500).json({ message: error });
@@ -51,39 +42,31 @@ const update = async (req, res) => {
 
 //for localdevelopment
 
-// const create = async (req, res) => {
-//     console.log('create method call');
+// const create = async (req, res) => {    
 //     try {
-//         client.query("INSERT INTO botconversations(uuid,created_at,modified_at,conversation)VALUES($1,now(),now(),$2) RETURNING *", [uuidv4(), req.body], (error, result) => {
+//         client.query("INSERT INTO botconversations(uuid,created_at,modified_at,conversation)VALUES($1,now(),now(),$2) RETURNING *", [uuidv4(), req.body], async(error, result) => {
 //             if (!error) {
+//                 await sendMail();
 //                 res.status(201).json({ data: result.rows[0] });
-//                 // if (sendMail())
-//                 //     res.status(201).json({ data: result.rows[0] });
-//                 // else {
-//                 //     res.status(500).json({ message: "Something went wron on email sent" });
-//                 // }
+                            
 //             }
 //             else {
 //                 res.status(500).json({ message: error });
-//             }            
+//             }
+
 //         });
 //     } catch (error) {
 //         console.log('catch error');
 //         res.status(500).json({ message: error });
 //     }
 // };
-// const update = async (req, res) => {
-//     console.log('update method call');
-//     const { uuid } = req.params;    
+// const update = async (req, res) => {    
+//     const { uuid } = req.params;
 //     try {
-//          client.query("UPDATE botconversations SET modified_at=now(),conversation=$1 WHERE uuid = $2 RETURNING *", [req.body, uuid], (error, result) => {
+//         client.query("UPDATE botconversations SET modified_at=now(),conversation=$1 WHERE uuid = $2 RETURNING *", [req.body, uuid], async(error, result) => {
 //             if (!error) {
-//                 res.status(201).json({ data: result.rows[0] });
-//                 // if (sendMail())
-//                 //     res.status(201).json({ data: result.rows });
-//                 // else {
-//                 //     res.status(500).json({ message: "Something went wron on email sent" });
-//                 // }
+//                 await sendMail();
+//                 res.status(201).json({ data: result.rows[0] });                             
 //             }
 //             else {
 //                 res.status(500).json({ message: error });
