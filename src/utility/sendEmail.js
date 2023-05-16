9e3b699ea99f0931c857247520c62ce322cb8b8f
SureshSen <115155721+SureshSen@users.noreply.github.com>
2023-05-16 11:30:38 +0530

    Update sendEmail.js

--- src/utility/sendEmail.js
+++ src/utility/sendEmail.js
@@ -1,30 +1,31 @@
 const nodemailer = require('nodemailer');
-const sendMail = () => {
+const sendMail = async () => {
     console.log("inside sendmail function")
 
     let mailTransporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user:"xyz",
-            pass:"1123"
+            user: "websitesspl@gmail.com",
+            pass: "clpxvwdjoywiixvh"
         }
     });
     //sending credentials
 
     let mailDetails = {
-        from: "suresh.sen@sculptsoft.com",
-        to: "kumaarsuresh057@gmail.com",
+        from: "websitesspl@gmail.com",
+        to: "suresh.sen@sculptsoft.com",
         subject: "Test mail using cron job email",
         text: "Node js cron job email. Testing for learning."
     }
 
-    mailTransporter.sendMail(mailDetails, (error, data) => {
+    const res = await mailTransporter.sendMail(mailDetails, (error, data) => {
         console.log("inside mail transpoter sendmail");
-        if(error)
-        console.log("Error occure:",error);
+        if (error) console.log("Error occure:", error);
+
         else
-        console.log("Email sent success",data);
+            console.log("Email sent success", data);
     });
 
-}
+    return res;
+};
 module.exports = sendMail;
